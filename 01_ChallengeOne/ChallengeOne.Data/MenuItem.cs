c1ab10b6374e1257884dcc2531eccfe133accc28
Brett Sibley <basibley1991@gmail.com>
2022-04-05 16:54:30 -0400

    C1 Data and Repo

---
+++ 01_ChallengeOne/ChallengeOne.Data/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿public class MenuItem
+{
+    public MenuItem (){}
+
+    public MenuItem (int mealNumber, string mealName, string mealDescription, string listOfIngredients, decimal mealPrice)
+    {
+        MealNumber = mealNumber;
+        MealName = mealName;
+        MealDescription = mealDescription;
+        ListOfIngredients = listOfIngredients;
+        MealPrice = mealPrice;
+    }
+
+    public int MealNumber { get; set; }
+    public string MealName { get; set; }
+    public string MealDescription { get; set; }
+    public string ListOfIngredients { get; set; }
+    public decimal MealPrice { get; set; }
+}
---
+++ 01_ChallengeOne/ChallengeOne.Repository/MenuItemRepository.cs
@@ -0,0 +1,48 @@
+﻿public class MenuItemRepository
+{
+    private readonly List<MenuItem> _menuItemDatabase = new List<MenuItem>();
+    private int _count;
+    public bool AddMenuItemToMenu(MenuItem menuItem)
+    {
+        if (menuItem != null)
+        {
+            _count++;
+            menuItem.MealNumber = _count;
+            _menuItemDatabase.Add(menuItem);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public List<MenuItem> GetWholeMenu()
+    {
+        return _menuItemDatabase;
+    }
+
+    public MenuItem GetMenuItemByMealNumber(int mealNumber)
+    {
+        foreach (var menuItem in _menuItemDatabase)
+        {
+            if (menuItem.MealNumber == mealNumber)
+            {
+                return menuItem;
+            }
+        }
+        return null;
+    }
+    public bool RemoveMenuItemFromMenu(int mealNumber)
+    {
+        var menuItem = GetMenuItemByMealNumber(mealNumber);
+        if (menuItem != null)
+        {
+            _menuItemDatabase.Remove(menuItem);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
